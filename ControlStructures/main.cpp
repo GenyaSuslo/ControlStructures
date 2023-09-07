24772b496c6d9014f0b88e57a5421b9861b8b8ba
Book <Book@WIN-8CS3AUJICSU>
2023-09-07 22:12:32 +0300

    loops

---
+++ ControlStructures/main.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+using namespace std;
+
+
+//#define WHILE_1
+#define WHILE_2
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef WHILE_1
+	int i = 0;//счетчик цикла
+	int n;//колличество итерация
+	cout << "Введите колличество итераций: "; cin >> n;
+	while (i < n)
+	{
+		cout << ++i << " Hello" << endl;
+		//i++;
+	}
+#endif // WHILE_1
+
+	int n;
+	cout << "Введите колличество итераций: "; cin >> n;
+	while (n--)
+	{
+		cout << n << "\t";
+	}
+	cout << endl;
+
+
+
+}
